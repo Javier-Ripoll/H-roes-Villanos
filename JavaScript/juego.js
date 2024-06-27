3240cba70bcb2a2737e3701b7a26f21a392f051e
Javier Ripoll Escarabajal <javier2003.jr@gmail.com>
2024-06-27 08:32:14 +0200

    Update juego.js

--- JavaScript/juego.js
+++ JavaScript/juego.js
@@ -1,7 +1,7 @@
 let puntuacionJuego;
 let card;
 let array= [];
-let marcado;
+let marcado = [];
 function getrandomItgem(max){ //nuemero RANDOME
     return Math.floor(Math.random() * max)
 }
@@ -32,7 +32,7 @@ function pintarPanel(){
     //lista de src para ir cambiando cada vez que se recargue la pagina
     let imgsrc = ["./imagenes/spidermancard.jpg","./imagenes/supermanCard.jpg","./imagenes/ironmanCard.jpg","./imagenes/DeadPoolCard.jpg"];
     let randomNumber = 0;
-    for (let index = 0; index<parseInt(tamano)* parseInt(tamano); index++){
+    for (let index = 0; index < parseInt(tamano)* parseInt(tamano); index++){
         randomNumber = getrandomItgem(4);
         itembocabajo += `<div id="${imgsrc[randomNumber]}" class="itembocabajo"><img src="./imagenes/bocaAbajo.jpg" alt=""  width="100px"></div>`
         items +=`<div id="${imgsrc[randomNumber]}" class="item"><img src="${imgsrc[randomNumber]}" alt="" width="100px"></div>`
@@ -43,43 +43,19 @@ function pintarPanel(){
 }
 //Esta funcion esta principalmente para: Dependiendo la dificultad se visualizaran las cartas x segundos 
 function cuentaAtras(){
-    if (dificultad == "Baja"){
-        let tiempo = parseInt(document.getElementById('tiempo').value)-1
-        document.getElementById('tiempo').value =tiempo
-        document.getElementById('cartasbocabajo').style.zIndex ="1"
-        document.getElementById('juego').style.zIndex= "2"
-        if (tiempo == 0){
-                clearInterval(IDinterval)
-                document.getElementById('cartasbocabajo').style.zIndex ="2"
-                document.getElementById('juego').style.zIndex= "1"
-                document.getElementById('tiempo').value= "0"
-        }
+    let tiempo = parseInt(document.getElementById('tiempo').value) - 1;
+    document.getElementById('tiempo').value = tiempo;
 
-    }else if (dificultad == "Media"){
-        let tiempo = parseInt(document.getElementById('tiempo').value)-1
-        document.getElementById('tiempo').value =tiempo
-        document.getElementById('cartasbocabajo').style.zIndex ="1"
-        document.getElementById('juego').style.zIndex= "2"
-        if (tiempo == 0){
-                clearInterval(IDinterval)
-                document.getElementById('cartasbocabajo').style.zIndex ="2"
-                document.getElementById('juego').style.zIndex= "1"
-                document.getElementById('tiempo').value= "0"
-        }
-    }else{
-        let tiempo = parseInt(document.getElementById('tiempo').value)-1
-        document.getElementById('tiempo').value =tiempo
-        document.getElementById('cartasbocabajo').style.zIndex ="1"
-        document.getElementById('juego').style.zIndex= "2"
-        if (tiempo == 0){
-                clearInterval(IDinterval)
-                document.getElementById('cartasbocabajo').style.zIndex ="2"
-                document.getElementById('juego').style.zIndex= "1"
-                document.getElementById('tiempo').value= "0"
-        }
+    document.getElementById('cartasbocabajo').style.zIndex = "1";
+    document.getElementById('juego').style.zIndex = "2";
+
+    if (tiempo == 0) {
+        clearInterval(IDinterval);
+        document.getElementById('cartasbocabajo').style.zIndex = "2";
+        document.getElementById('juego').style.zIndex = "1";
+        document.getElementById('tiempo').value = "0";
+        programarEventos();
     }
-    //Despues de que se haya hecho la cuenta atras dependiendo de la dificultad se programan los eventos 
-    programarEventos()
 }
 //Aqui deberiamos pinchar la carta boca abajo
 function programarEventos(){
@@ -91,38 +67,38 @@ function programarEventos(){
 //Eventos del juego 
 function clickImagenes(event){
     let item = event.target;
-    card = event.target.parentElement
-    marcado= []
-    //cada vez que se hace click se cambia
-    item.src = ""
-    ComprobacionDeCartas(card,marcado,item)
-}
-function ComprobacionDeCartas(card,marcado,item){
-   //ponemos los src en distintos arrays para luego comparar
-    if (marcado.length === 0) {
-        marcado.push(card.id)
-        array.push(marcado)
-    }else {
-        marcado.push(card.id)  
-    }
+    let card = item.parentElement;
+    item.src = card.id;
+    marcado.push(card);
 
-
-    if (array.length == 2){
-        let firstCard = document.getElementById(array[0]);
-        let SecondCard =document.getElementById(array[1]);
-        
-        // cogemos del id solamente la imagen
-        let firstCardImg= firstCard.querySelector('img');
-        let SecondCardImg = SecondCard.querySelector('img') ;
-        if (firstCardImg !== SecondCardImg){
-            firstCard.querySelector('img').src = "./imagenes/bocaAbajo.jpg";
-            SecondCard.querySelector('img').src = "./imagenes/bocaAbajo.jpg"
+    if(marcado.length === 2)
+        {
+            ComprobacionDeCartas();
         }
+}
+function ComprobacionDeCartas(){
+
+    let primeraImg = marcado[0].querySelector('img');
+    let segundaImg = marcado[1].querySelector('img');
+    console.log(primeraImg);
+    if (primeraImg && segundaImg) {
+        // Comparar las fuentes (src) de las imágenes
+        if (primeraImg.src === segundaImg.src) {
+            // Las imágenes son iguales, coincidencia encontrada
+            console.log('¡Pareja encontrada!');
+        } else {
+            // Las imágenes no son iguales, volver a ocultarlas después de un tiempo
+            setTimeout(() => {
+                primeraImg.src = "./imagenes/bocaAbajo.jpg";
+                segundaImg.src = "./imagenes/bocaAbajo.jpg";
+            }, 1000); // Volver a tapar después de 1 segundo (1000 milisegundos)
+        }
+    } else {
+        console.error("No se encontraron elementos img dentro de las cartas marcadas");
     }
-    if (array.length >1) {
-            array=[];       
-    }
-    console.log(array);
+
+    // Limpiar el array de cartas marcadas para la siguiente comparación
+    marcado = [];
     
 }
 //capturamos Datos
@@ -136,4 +112,3 @@ pintarPanel()
 //Comenzar Juego
 
 //Crear eventos del juego 
-
